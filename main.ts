036644ee39f5f6d0260aad601ec774b2a49fe699
AdamCernohous <71689826+AdamCernohous@users.noreply.github.com>
2021-04-02 19:22:18 +0200

    levels

--- main.ts
+++ main.ts
@@ -1,13 +1,4 @@
 scene.setBackgroundColor(15)
-tiles.setTilemap(tilemap`level1`)
-
-tiles.placeOnTile(player, tiles.getTileLocation(1, 35))
-
-tiles.setTileAt(tiles.getTileLocation(0, 30), assets.tile`enemySpawner`)
-tiles.setTileAt(tiles.getTileLocation(0, 21), assets.tile`enemySpawner`)
-tiles.setTileAt(tiles.getTileLocation(11, 17), assets.tile`enemySpawner`)
-tiles.setTileAt(tiles.getTileLocation(2, 2), assets.tile`enemySpawner`)
-tiles.setTileAt(tiles.getTileLocation(8, 2), assets.tile`bossSpawner`)
 
 let enemy = sprites.create(img`
         . . . . . . . e e e e . . . . . 
@@ -27,78 +18,50 @@ let enemy = sprites.create(img`
         . 4 5 4 5 5 4 e . . . . . . . . 
         . . 4 4 e e e . . . . . . . . . 
         `, SpriteKind.Enemy)
-enemy.follow(player, 50)
-
-for (let value of tiles.getTilesByType(assets.tile`enemySpawner`)) {
-    let enemy = sprites.create(img`
-        . . . . . . . e e e e . . . . . 
-        . . . . . e e 4 5 5 5 e e . . . 
-        . . . . e 4 5 6 2 2 7 6 6 e . . 
-        . . . e 5 6 6 7 2 2 6 4 4 4 e . 
-        . . e 5 2 2 7 6 6 4 5 5 5 5 4 . 
-        . e 5 6 2 2 8 8 5 5 5 5 5 4 5 4 
-        . e 5 6 7 7 8 5 4 5 4 5 5 5 5 4 
-        e 4 5 8 6 6 5 5 5 5 5 5 4 5 5 4 
-        e 5 c e 8 5 5 5 4 5 5 5 5 5 5 4 
-        e 5 c c e 5 4 5 5 5 4 5 5 5 e . 
-        e 5 c c 5 5 5 5 5 5 5 5 4 e . . 
-        e 5 e c 5 4 5 4 5 5 5 e e . . . 
-        e 5 e e 5 5 5 5 5 4 e . . . . . 
-        4 5 4 e 5 5 5 5 e e . . . . . . 
-        . 4 5 4 5 5 4 e . . . . . . . . 
-        . . 4 4 e e e . . . . . . . . . 
-        `, SpriteKind.Enemy)
-    tiles.placeOnRandomTile(enemy, assets.tile`enemySpawner`)
-    enemy.follow(player, 50)
-}
+enemy.destroy()
 
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     game.over(false)
 })
 
+nextLevel()
 
-for(let i = 0; i < 5; i++){
-    let boss = sprites.create(img`
-    . . . . c c c b b b b b . . . . 
-    . . c c b 4 4 4 4 4 4 b b b . . 
-    . c c 4 4 4 4 4 5 4 4 4 4 b c . 
-    . e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
-    e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
-    e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
-    e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
-    . e b 4 4 4 4 4 5 4 4 4 4 b e . 
-    8 7 e e b 4 4 4 4 4 4 b e e 6 8 
-    8 7 2 e e e e e e e e e e 2 7 8 
-    e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
-    e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
-    e b e 8 8 c c 8 8 c c c 8 e b e 
-    e e b e c c e e e e e c e b e e 
-    . e e b b 4 4 4 4 4 4 4 4 e e . 
-    . . . c c c c c e e e e e . . . 
-    `, SpriteKind.Player)
-    tiles.placeOnTile(boss, tiles.getTileLocation(0, 2))
-    boss.follow(player, randint(20, 50))
+let currentLevel = 1
+function nextLevel () {
+    currentLevel += 1
+    if (currentLevel == 1) {
+        tiles.setTilemap(tilemap`level1`)
+    }
+    tiles.setTilemap(tilemap`level1`)
+
+    tiles.placeOnTile(player, tiles.getTileLocation(1, 35))
+
+    tiles.setTileAt(tiles.getTileLocation(0, 30), assets.tile`enemySpawner`)
+    tiles.setTileAt(tiles.getTileLocation(0, 21), assets.tile`enemySpawner`)
+    tiles.setTileAt(tiles.getTileLocation(11, 17), assets.tile`enemySpawner`)
+    tiles.setTileAt(tiles.getTileLocation(2, 2), assets.tile`enemySpawner`)
+    tiles.setTileAt(tiles.getTileLocation(8, 2), assets.tile`bossSpawner`)
+
+    for (let value of tiles.getTilesByType(assets.tile`enemySpawner`)) {
+        let enemy = sprites.create(img`
+            . . . . . . . e e e e . . . . . 
+            . . . . . e e 4 5 5 5 e e . . . 
+            . . . . e 4 5 6 2 2 7 6 6 e . . 
+            . . . e 5 6 6 7 2 2 6 4 4 4 e . 
+            . . e 5 2 2 7 6 6 4 5 5 5 5 4 . 
+            . e 5 6 2 2 8 8 5 5 5 5 5 4 5 4 
+            . e 5 6 7 7 8 5 4 5 4 5 5 5 5 4 
+            e 4 5 8 6 6 5 5 5 5 5 5 4 5 5 4 
+            e 5 c e 8 5 5 5 4 5 5 5 5 5 5 4 
+            e 5 c c e 5 4 5 5 5 4 5 5 5 e . 
+            e 5 c c 5 5 5 5 5 5 5 5 4 e . . 
+            e 5 e c 5 4 5 4 5 5 5 e e . . . 
+            e 5 e e 5 5 5 5 5 4 e . . . . . 
+            4 5 4 e 5 5 5 5 e e . . . . . . 
+            . 4 5 4 5 5 4 e . . . . . . . . 
+            . . 4 4 e e e . . . . . . . . . 
+            `, SpriteKind.Enemy)
+        tiles.placeOnRandomTile(enemy, assets.tile`enemySpawner`)
+        enemy.follow(player, 50)
+    }
 }
-
-/*
-let boss = sprites.create(img`
-    . . . . c c c b b b b b . . . . 
-    . . c c b 4 4 4 4 4 4 b b b . . 
-    . c c 4 4 4 4 4 5 4 4 4 4 b c . 
-    . e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
-    e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
-    e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
-    e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
-    . e b 4 4 4 4 4 5 4 4 4 4 b e . 
-    8 7 e e b 4 4 4 4 4 4 b e e 6 8 
-    8 7 2 e e e e e e e e e e 2 7 8 
-    e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
-    e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
-    e b e 8 8 c c 8 8 c c c 8 e b e 
-    e e b e c c e e e e e c e b e e 
-    . e e b b 4 4 4 4 4 4 4 4 e e . 
-    . . . c c c c c e e e e e . . . 
-`, SpriteKind.Player)
-tiles.placeOnTile(boss, tiles.getTileLocation(0, 2))
-boss.follow(player, randint(20, 50))
-*/
