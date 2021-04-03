e2b234e697b4213716b5082e1b354b9be562cf5d
AdamCernohous <71689826+AdamCernohous@users.noreply.github.com>
2021-04-03 14:03:04 +0200

    level2

--- enemy.ts
+++ enemy.ts
@@ -1,3 +1,48 @@
+let enemy = sprites.create(img`
+    . . . . . . . e e e e . . . . . 
+    . . . . . e e 4 5 5 5 e e . . . 
+    . . . . e 4 5 6 2 2 7 6 6 e . . 
+    . . . e 5 6 6 7 2 2 6 4 4 4 e . 
+    . . e 5 2 2 7 6 6 4 5 5 5 5 4 . 
+    . e 5 6 2 2 8 8 5 5 5 5 5 4 5 4 
+    . e 5 6 7 7 8 5 4 5 4 5 5 5 5 4 
+    e 4 5 8 6 6 5 5 5 5 5 5 4 5 5 4 
+    e 5 c e 8 5 5 5 4 5 5 5 5 5 5 4 
+    e 5 c c e 5 4 5 5 5 4 5 5 5 e . 
+    e 5 c c 5 5 5 5 5 5 5 5 4 e . . 
+    e 5 e c 5 4 5 4 5 5 5 e e . . . 
+    e 5 e e 5 5 5 5 5 4 e . . . . . 
+    4 5 4 e 5 5 5 5 e e . . . . . . 
+    . 4 5 4 5 5 4 e . . . . . . . . 
+    . . 4 4 e e e . . . . . . . . . 
+`, SpriteKind.Enemy)
+enemy.follow(player, 50)
+
+for (let i of tiles.getTilesByType(assets.tile`enemySpawner`)) {
+    let enemy = sprites.create(img`
+        . . . . . . . e e e e . . . . . 
+        . . . . . e e 4 5 5 5 e e . . . 
+        . . . . e 4 5 6 2 2 7 6 6 e . . 
+        . . . e 5 6 6 7 2 2 6 4 4 4 e . 
+        . . e 5 2 2 7 6 6 4 5 5 5 5 4 . 
+        . e 5 6 2 2 8 8 5 5 5 5 5 4 5 4 
+        . e 5 6 7 7 8 5 4 5 4 5 5 5 5 4 
+        e 4 5 8 6 6 5 5 5 5 5 5 4 5 5 4 
+        e 5 c e 8 5 5 5 4 5 5 5 5 5 5 4 
+        e 5 c c e 5 4 5 5 5 4 5 5 5 e . 
+        e 5 c c 5 5 5 5 5 5 5 5 4 e . . 
+        e 5 e c 5 4 5 4 5 5 5 e e . . . 
+        e 5 e e 5 5 5 5 5 4 e . . . . . 
+        4 5 4 e 5 5 5 5 e e . . . . . . 
+        . 4 5 4 5 5 4 e . . . . . . . . 
+        . . 4 4 e e e . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    tiles.placeOnTile(enemy, i)
+    enemy.follow(player, 50)
+}
+tiles.placeOnTile(enemy, tiles.getTileLocation(0, 0))
+enemy.follow(player, 50)
+
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, otherSprite) {
     sprite.destroy(effects.fire, 100)
     otherSprite.destroy()
--- main.ts
+++ main.ts
@@ -1,24 +1,6 @@
-scene.setBackgroundColor(15)
+let currentLevel = 1
 
-let enemy = sprites.create(img`
-        . . . . . . . e e e e . . . . . 
-        . . . . . e e 4 5 5 5 e e . . . 
-        . . . . e 4 5 6 2 2 7 6 6 e . . 
-        . . . e 5 6 6 7 2 2 6 4 4 4 e . 
-        . . e 5 2 2 7 6 6 4 5 5 5 5 4 . 
-        . e 5 6 2 2 8 8 5 5 5 5 5 4 5 4 
-        . e 5 6 7 7 8 5 4 5 4 5 5 5 5 4 
-        e 4 5 8 6 6 5 5 5 5 5 5 4 5 5 4 
-        e 5 c e 8 5 5 5 4 5 5 5 5 5 5 4 
-        e 5 c c e 5 4 5 5 5 4 5 5 5 e . 
-        e 5 c c 5 5 5 5 5 5 5 5 4 e . . 
-        e 5 e c 5 4 5 4 5 5 5 e e . . . 
-        e 5 e e 5 5 5 5 5 4 e . . . . . 
-        4 5 4 e 5 5 5 5 e e . . . . . . 
-        . 4 5 4 5 5 4 e . . . . . . . . 
-        . . 4 4 e e e . . . . . . . . . 
-        `, SpriteKind.Enemy)
-enemy.destroy()
+scene.setBackgroundColor(15)
 
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     game.over(false)
@@ -26,42 +8,16 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
 
 nextLevel()
 
-let currentLevel = 1
 function nextLevel () {
-    currentLevel += 1
     if (currentLevel == 1) {
         tiles.setTilemap(tilemap`level1`)
-    }
-    tiles.setTilemap(tilemap`level1`)
+        tiles.placeOnTile(player, tiles.getTileLocation(1, 35))
 
-    tiles.placeOnTile(player, tiles.getTileLocation(1, 35))
+        tiles.setTileAt(tiles.getTileLocation(0, 30), assets.tile`enemySpawner`)
+        tiles.setTileAt(tiles.getTileLocation(0, 21), assets.tile`enemySpawner`)
+        tiles.setTileAt(tiles.getTileLocation(11, 17), assets.tile`enemySpawner`)
+        tiles.setTileAt(tiles.getTileLocation(2, 2), assets.tile`enemySpawner`)
 
-    tiles.setTileAt(tiles.getTileLocation(0, 30), assets.tile`enemySpawner`)
-    tiles.setTileAt(tiles.getTileLocation(0, 21), assets.tile`enemySpawner`)
-    tiles.setTileAt(tiles.getTileLocation(11, 17), assets.tile`enemySpawner`)
-    tiles.setTileAt(tiles.getTileLocation(2, 2), assets.tile`enemySpawner`)
-    tiles.setTileAt(tiles.getTileLocation(8, 2), assets.tile`bossSpawner`)
-
-    for (let value of tiles.getTilesByType(assets.tile`enemySpawner`)) {
-        let enemy = sprites.create(img`
-            . . . . . . . e e e e . . . . . 
-            . . . . . e e 4 5 5 5 e e . . . 
-            . . . . e 4 5 6 2 2 7 6 6 e . . 
-            . . . e 5 6 6 7 2 2 6 4 4 4 e . 
-            . . e 5 2 2 7 6 6 4 5 5 5 5 4 . 
-            . e 5 6 2 2 8 8 5 5 5 5 5 4 5 4 
-            . e 5 6 7 7 8 5 4 5 4 5 5 5 5 4 
-            e 4 5 8 6 6 5 5 5 5 5 5 4 5 5 4 
-            e 5 c e 8 5 5 5 4 5 5 5 5 5 5 4 
-            e 5 c c e 5 4 5 5 5 4 5 5 5 e . 
-            e 5 c c 5 5 5 5 5 5 5 5 4 e . . 
-            e 5 e c 5 4 5 4 5 5 5 e e . . . 
-            e 5 e e 5 5 5 5 5 4 e . . . . . 
-            4 5 4 e 5 5 5 5 e e . . . . . . 
-            . 4 5 4 5 5 4 e . . . . . . . . 
-            . . 4 4 e e e . . . . . . . . . 
-            `, SpriteKind.Enemy)
-        tiles.placeOnRandomTile(enemy, assets.tile`enemySpawner`)
-        enemy.follow(player, 50)
+        tiles.setTileAt(tiles.getTileLocation(1, 3), assets.tile`lvl1end`)
     }
 }
